15af97de8d885b8d2d20064f215ea458d6218df5
bivhunter <bivhunter@ukr.net>
2019-12-03 18:48:41 +0200

    implement TaskList

---
+++ components/TaskListComponent.js
@@ -0,0 +1,39 @@
+import {TaskComponent} from "./TaskComponent.js";
+
+
+export class TaskListComponent extends HTMLElement {
+    constructor(props = {taskList: []}){
+        super();
+        this._props = props;
+        this.onInit();
+    }
+    
+    onInit() {
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        const div = document.createElement('div');
+        const ul = document.createElement('ul');
+
+        this._props.taskList.forEach((task) => {
+           let li = document.createElement('li');
+
+           let taskComponent = new TaskComponent(task);
+           li.appendChild(taskComponent);
+           ul.appendChild(li);
+        });
+
+
+        div.appendChild(ul);
+        this.shadowRoot.appendChild(div);
+
+    }
+
+
+
+}
